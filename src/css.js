1e3a40f9ed282a4c8bc35d50facb95ad3783e367
Mr21 <thomastortorini@gmail.com>
2014-03-16 15:58:26 -0400

    .css(), "[+-]=", coding style + not forget to accept the strings without unity at all (like "+=10", it's px by default).

--- src/css.js
+++ src/css.js
@@ -247,39 +247,43 @@ jQuery.extend({
 	// * prop : String, the CSS property.
 	pixelsToUnity: function( px, unit, elem, prop ) {
 		switch ( unit ) {
-			case "px" : return px; // Don't waste our time if there is no conversion to do.
-			case "em" : return px / jQuery.css(elem, "fontSize", ""); // "em" refers to the fontSize of the current element.
-			case "%"  :
-				if (/^(left$|right$|margin|padding)/.test(prop)) {
-					prop = "width";
-				} else if (/^(top|bottom)$/.test(prop)) {
-					prop = "height";
+		case "":
+		case "px":
+			return px; // Don't waste our time if there is no conversion to do.
+		case "em":
+			return px / jQuery.css( elem, "fontSize", "" ); // "em" refers to the fontSize of the current element.
+		case "%":
+			if ( /^(left$|right$|margin|padding)/.test( prop ) ) {
+				prop = "width";
+			} else if ( /^(top|bottom)$/.test( prop ) ) {
+				prop = "height";
+			}
+			elem = /^(relative|absolute|fixed)$/.test( jQuery.css( elem, "position" ) ) ?
+				elem.offsetParent : elem.parentNode;
+			if ( elem ) {
+				prop = jQuery.css( elem, prop, true );
+				if ( prop !== 0 ) {
+					return px / prop * 100;
 				}
-				elem = /^(relative|absolute|fixed)$/.test(jQuery.css(elem, "position")) ? elem.offsetParent : elem.parentNode;
-				if (elem) {
-					prop = jQuery.css(elem, prop, "");
-					if (jQuery.isNumeric(prop) && prop !== 0) {
-						return px / prop * 100;
-					}
-				}
-				return 0;
+			}
+			return 0;
 		}
 		// The first time we calculate how many pixels there is in 1 meter
 		// for calculate what is 1 inch/cm/mm/etc.
 		if ( jQuery.pixelsToUnity.units === undefined ) {
 			var units = jQuery.pixelsToUnity.units = {},
-				div = document.createElement("div");
+				div = document.createElement( "div" );
 			div.style.width = "100cm";
-			document.body.appendChild(div); // If we don't link the <div> to something, the offsetWidth attribute will be not set correctly.
+			document.body.appendChild( div ); // If we don't link the <div> to something, the offsetWidth attribute will be not set correctly.
 			units.mm = div.offsetWidth / 1000;
-			document.body.removeChild(div);
+			document.body.removeChild( div );
 			units.cm = units.mm * 10;
 			units.in = units.cm * 2.54;
 			units.pt = units.in * 1 / 72;
 			units.pc = units.pt * 12;
 		}
 		// If the unity specified is not recognized we return the value.
-		unit = jQuery.pixelsToUnity.units[unit];
+		unit = jQuery.pixelsToUnity.units[ unit ];
 		return unit ? px / unit : px;
 	},
 
@@ -320,7 +324,7 @@ jQuery.extend({
 
 			// If a number was passed in, add 'px' to the (except for certain CSS properties)
 			if ( type === "number" && !jQuery.cssNumber[ origName ] ) {
-				value += ret ? ret[3] : "px";
+				value += ret && ret[3] ? ret[3] : "px";
 			}
 
 			// Fixes #8908, it can be done more correctly by specifying setters in cssHooks,
